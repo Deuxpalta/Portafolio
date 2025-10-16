2d26e50f909f876d10de5c8aee758cb51604f200
Deuxpalta <gabrieleduardoveraf7@gmail.com>
2025-10-16 19:25:10 -0300

    first_fixes
    primeros arreglos a una serie de problemas

--- portafolio/src/pages/AboutMe.jsx
+++ portafolio/src/pages/AboutMe.jsx
@@ -1,22 +1,3 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import aboutMeItems from '../data/AboutMe';
-import AboutMeCard from '../components/organisms/NewsCard';
-
-
-function AboutMe() {
- return (
-   <Container className="my-5">
-     <h1>Acerca de Mí</h1>
-    <Row className="justify-content-center">
-      {aboutMeItems.map((aboutMeItem) => (
-        <Col key={aboutMeItem.id} xs={12} sm={6} md={4} lg={3} className="d-flex justify-content-center">
-          <AboutMeCard aboutMeItem={aboutMeItem} />
-        </Col>
-      ))}
-    </Row>
-   </Container>
- );
-}
-
-
+// AboutMe page removed. Keep a harmless default export so any imports elsewhere don't break.
+const AboutMe = null;
 export default AboutMe;
